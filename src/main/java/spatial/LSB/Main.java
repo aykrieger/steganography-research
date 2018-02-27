73d486ab8fa592cb157883448cb698de23ade8f1
James <james.thomas.vaughn@gmail.com>
2018-02-26 21:36:29 -0500

    Added command line parsing to lsb. LSB should be done

--- src/main/java/spatial/LSB/Main.java
+++ src/main/java/spatial/LSB/Main.java
@@ -1,38 +1,79 @@
 package spatial.LSB;
 
+import org.apache.commons.cli.*;
 import java.io.*;
+
 
 public class Main {
 
-    //TODO make image file and message a cmdline parameter
     public static void main( String[] args ) {
-        StringBuilder message = new StringBuilder();
 
-        // get message to encode
-        try (BufferedReader reader = new BufferedReader(new FileReader("Messages/message.txt"))){
-            String line;
+        Options options = CreateCmdLineOptions();
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
 
-            while((line = reader.readLine()) != null) {
-                message.append(line);
-                message.append("\n");
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("LSB encoder", options);
+
+            System.exit(1);
+            return;
+        }
+
+        String inputFilePath = cmd.getOptionValue("input");
+        String outputFilePath = cmd.getOptionValue("output");
+        String messageText = cmd.getOptionValue("message") + '\0';
+
+        if (cmd.hasOption("encode")) {
+            try {
+                EncodeMessage(inputFilePath,
+                        outputFilePath,
+                        messageText);
+            } catch(IOException e) {
+                System.err.println(e);
             }
-        } catch(IOException e) {
-            System.err.println(e);
+        }
+        else if(cmd.hasOption("decode")) {
+            try {
+                DecodeMessage(outputFilePath);
+            } catch(IOException e) {
+                System.err.println(e);
+            }
         }
 
-        try {
-            EncodeMessage("InputImages/lsb.png",
-                    "OutputImages/lsb_out.png",
-                    message.toString());
-        } catch(IOException e) {
-            System.err.println(e);
+        else {
+            System.out.println("No action selected:\n");
+            formatter.printHelp("LSB encoder", options);
         }
+    }
 
-        try {
-            DecodeMessage("OutputImages/lsb_out.png");
-        } catch(IOException e) {
-            System.err.println(e);
-        }
+    private static Options CreateCmdLineOptions() {
+        Options options = new Options();
+
+        Option input = new Option("i", "input", true, "input image path");
+        input.setRequired(true);
+        options.addOption(input);
+
+        Option output = new Option("o", "output", true, "output image path");
+        output.setRequired(false);
+        options.addOption(output);
+
+        Option message = new Option("m", "message", true, "stego message");
+        output.setRequired(false);
+        options.addOption(message);
+
+        Option encode = new Option("e", "encode", false, "encode message");
+        output.setRequired(false);
+        options.addOption(encode);
+
+        Option decode = new Option("d", "decode", false, "decode message");
+        output.setRequired(false);
+        options.addOption(decode);
+
+        return options;
     }
 
     public static void EncodeMessage(String inputFileName, String outputFileName, String message) throws IOException{
