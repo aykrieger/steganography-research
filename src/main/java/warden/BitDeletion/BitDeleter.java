b292d9b4dbac09e4893afc76a348ec41bbaab133
James <james.thomas.vaughn@gmail.com>
2018-02-27 00:00:35 -0500

    Finished the bit deletion warden

---
+++ src/main/java/warden/BitDeletion/BitDeleter.java
@@ -0,0 +1,55 @@
+package warden.BitDeletion;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+
+/**
+ * Performs bit deletion on a stego image as described by Smith and Agaian in "Denoising and the Active Warden"
+ * It sets the last 4 bit planes of each pixel's RGB values to 0, thus removing any messages hidden in the noise
+ */
+public class BitDeleter {
+    private String imageFileName;
+    private Optional<BufferedImage> stegoImage = Optional.empty();
+
+    public BitDeleter(String imageFileName) {
+        this.imageFileName = imageFileName;
+    }
+
+    public boolean WriteImage(String outputFileName) throws IOException {
+        if(this.stegoImage.isPresent() == false) {
+            return false;
+        }
+
+        File outputImageFile = new File(outputFileName);
+        ImageIO.write(this.stegoImage.get(), "png", outputImageFile);
+
+        return true;
+    }
+
+    public void ScrubImage() throws IOException {
+        BufferedImage image = ImageIO.read(new File(this.imageFileName));
+
+        for (int y = 0; y < image.getHeight(); y ++) {
+            for (int x = 0; x < image.getWidth(); x ++) {
+                int pixelColor = image.getRGB(x,y);
+
+                // Remove the red last 4 LSBs
+                pixelColor = (pixelColor & 0xfff0ffff);
+
+                // Remove the green last 4 LSBs
+                pixelColor = (pixelColor & 0xfffff0ff);
+
+                // Remove the blue last 4 LSBs
+                pixelColor = (pixelColor & 0xfffffff0);
+
+                // Save the change
+                image.setRGB(x, y, pixelColor);
+            }
+        }
+
+        this.stegoImage = Optional.of(image);
+    }
+}
---
+++ src/main/java/warden/BitDeletion/Main.java
@@ -0,0 +1,56 @@
+package warden.BitDeletion;
+
+import org.apache.commons.cli.*;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Options options = CreateCmdLineOptions();
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("Bit deletion warden", options);
+
+            System.exit(1);
+            return;
+        }
+
+        String inputFilePath = cmd.getOptionValue("input");
+        String outputFilePath = cmd.getOptionValue("output");
+
+        try {
+            RunWarden(inputFilePath, outputFilePath);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    private static Options CreateCmdLineOptions() {
+        Options options = new Options();
+
+        Option input = new Option("i", "input", true, "input image path");
+        input.setRequired(true);
+        options.addOption(input);
+
+        Option output = new Option("o", "output", true, "output image path");
+        output.setRequired(false);
+        options.addOption(output);
+
+        return options;
+    }
+
+    private static void RunWarden(String inputFilePath, String outputFilePath) throws IOException {
+        BitDeleter warden = new BitDeleter(inputFilePath);
+        warden.ScrubImage();
+        warden.WriteImage(outputFilePath);
+    }
+}
