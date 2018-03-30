85ac78d55dc0441f211d8f14dd98cfb5379fdf7c
James <james.thomas.vaughn@gmail.com>
2018-03-30 16:52:12 -0400

    Added LSB encoding to DWT

--- src/main/java/frequency/DWT/DWTEncoder.java
+++ src/main/java/frequency/DWT/DWTEncoder.java
@@ -5,6 +5,7 @@ import jwave.transforms.FastWaveletTransform;
 import jwave.transforms.wavelets.haar.Haar1;
 import lib.BitBuilder;
 import lib.BitIterator;
+import lib.TypeConverter;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -18,14 +19,12 @@ import java.util.Optional;
  * Uses a 2D Haar Wavelet transform to encode message bits
  */
 
-// TODO replace variable names and properly implement
 public class DWTEncoder {
     private String imageFileName;
     private Optional<BufferedImage> stegoImage = Optional.empty();
     private int p = 2; //block width
     private int k = 6; //for case 3, we will embed bits in all segments of the 3 x 2 block
 
-    //TODO Allow different embedding strategies? Can choose strategy from Enum and set p and k based off that
     public DWTEncoder(String imageFileName) {
         this.imageFileName = imageFileName;
 
@@ -42,9 +41,6 @@ public class DWTEncoder {
         return true;
     }
 
-    //TODO implement
-    //TODO Allow different embedding strategies?
-
     /**
      * Encodes the hidden message using Case 3 for now
      * We are handling the encryption separately, so we do not need to worry about the Cantor Encryption Strategy
@@ -56,6 +52,8 @@ public class DWTEncoder {
         BitIterator B = new BitIterator(message);
 
         BufferedImage C = ImageIO.read(new File(this.imageFileName)); //size is MxN
+        int width = C.getWidth();
+        int height = C.getHeight();
 
         //Embed all of the message data into the transformed image blocks
 
@@ -68,13 +66,27 @@ public class DWTEncoder {
         //      for segment <- block:
         //          segment = (segment & component) | (nextBit(B) << offset)
 
+        int[][] coefficients = forwardHaar(C);
+
+        encodingLoop:
+        for(int j = height / 2; j < height; j++) {
+            for(int i = width/2; i < width; i++) {
+                if(B.hasNext() == false) break encodingLoop;
+
+                coefficients[i][j] = (coefficients[i][j] & 0xFFFFFFFE) | B.next();
+
+                System.out.println(coefficients[i][j]);
+            }
+        }
+
         //Store the inverted image
         //Inverted image is the final stego image with the message embedded in the DWT values but not the final RGB
-        //this.stegoImage = Optional.of(reverseHaar(C, forwardHaar(C)));
-        this.stegoImage = Optional.of(visualizeHaar(C));
+        this.stegoImage = Optional.of(reverseHaar(C, coefficients));
+
+        test(coefficients, height, width);
     }
 
-    private double[][] forwardHaar(BufferedImage C) {
+    private int[][] forwardHaar(BufferedImage C) {
         double[][] pixelData = new double[C.getHeight()][C.getWidth()];
 
         //get the pixel data to transform
@@ -88,13 +100,15 @@ public class DWTEncoder {
 
         //apply the haar function
         Transform t = new Transform( new FastWaveletTransform( new Haar1()));
-        return t.forward(pixelData, 1, 1);
+        pixelData = t.forward(pixelData, 1, 1);
+
+        return TypeConverter.doubleMatrixToIntMatrix(pixelData);
     }
 
-    private BufferedImage reverseHaar(BufferedImage C, double[][] coefficients) {
+    private BufferedImage reverseHaar(BufferedImage C, int[][] coefficients) {
         //apply the haar function
         Transform t = new Transform( new FastWaveletTransform( new Haar1()));
-        double[][] pixelData = t.reverse(coefficients, 1, 1);
+        double[][] pixelData = t.reverse(TypeConverter.intMatrixToDoubleMatrix(coefficients), 1, 1);
 
         for (int row = 0; row < C.getHeight(); row++) {
             for (int col = 0; col < C.getWidth(); col++) {
@@ -108,36 +122,46 @@ public class DWTEncoder {
         return C;
     }
 
-    private BufferedImage visualizeHaar(BufferedImage C) {
-        double[][] pixelData = new double[C.getHeight()][C.getWidth()];
+    //TODO Remove
+    //Tests how the transform affects the coefficients
+    private void test(int[][] coefficients, int height, int width) {
+        Transform t = new Transform( new FastWaveletTransform( new Haar1()));
+        double[][] pixelData = t.reverse(TypeConverter.intMatrixToDoubleMatrix(coefficients), 1, 1);
 
-        //get the pixel data to transform
-        for (int row = 0; row < C.getHeight(); row++) {
-            for (int col = 0; col < C.getWidth(); col++) {
-                //for now, just take the B plane
-                //TODO
-                pixelData[col][row] = C.getRGB(col, row) & 0x000000FF;
+        coefficients = TypeConverter.doubleMatrixToIntMatrix(t.forward(pixelData, 1, 1));
+        BitBuilder result = new BitBuilder();
+        int b = 0x00;
+
+        encodingLoop:
+        for(int j = height / 2; j < height; j++) {
+            for(int i = width/2; i < width; i++) {
+                System.out.println(coefficients[i][j]);
+
+                b = (coefficients[i][j] & 0x00000001);
+
+                if(result.append((byte) b)) break encodingLoop;
             }
         }
 
+        //return final string formed from bytes
+        System.out.println(result.toString());
+    }
 
-        //apply the haar function
-        Transform t = new Transform( new FastWaveletTransform( new Haar1()));
-        pixelData = t.forward(pixelData, 1, 1);
-        double max = pixelData[0][0];
-        for(int i = 0; i < pixelData.length; i++) {
-            for(int j = 0; j < pixelData[0].length; j++) {
-                if(max < pixelData[i][j]) {
-                    max = pixelData[i][j];
+    private BufferedImage visualizeHaar(BufferedImage C, int[][] coeffs) {
+        double max = coeffs[0][0];
+        for(int i = 0; i < coeffs.length; i++) {
+            for(int j = 0; j < coeffs[0].length; j++) {
+                if(max < coeffs[i][j]) {
+                    max = coeffs[i][j];
                 }
             }
         }
         //create the haar image
-        for (int row = 0; row < C.getHeight(); row++) {
-            for (int col = 0; col < C.getWidth(); col++) {
+        for (int row = 0; row < coeffs.length; row++) {
+            for (int col = 0; col < coeffs[0].length; col++) {
                 //for now, just take the B plane
                 //TODO
-                double val = 255 * (pixelData[col][row] / max);
+                double val = 255 * (coeffs[col][row] / max);
                 int newColor = 0xFF000000 + ((int) val << 16) + ((int) val << 8) + ((int) val);
                 C.setRGB(col, row, newColor);
             }
@@ -146,11 +170,13 @@ public class DWTEncoder {
         return C;
     }
 
-    //TODO implement
     public String Decode() throws IOException {
         BitBuilder result = new BitBuilder();
         int b = 0x00;
+
         BufferedImage S = ImageIO.read(new File(this.imageFileName));
+        int width = S.getWidth();
+        int height = S.getHeight();
 
         //for component <- {R: 0x00010000, G: 0x00000100, B: 0x00000001}
         //    offset <- {R: 16, G: 8, B: 0}:
@@ -161,7 +187,20 @@ public class DWTEncoder {
         //      for segment <- block:
         //          result.append(segment & component) >> offset)
 
+        int[][] coefficients = forwardHaar(S);
+
+        encodingLoop:
+        for(int j = height / 2; j < height; j++) {
+            for(int i = width/2; i < width; i++) {
+                System.out.println(coefficients[i][j]);
+
+                b = (coefficients[i][j] & 0x00000001);
+
+                if(result.append((byte) b)) break encodingLoop;
+            }
+        }
+
         //return final string formed from bytes
-        return null;
+        return result.toString();
     }
 }
