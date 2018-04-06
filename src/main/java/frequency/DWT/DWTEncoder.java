18dc2aada0c3865dd43c4d1c47b45642810531d0
James <james.thomas.vaughn@gmail.com>
2018-04-06 00:18:19 -0400

    Have haar DWT encoder working now

--- src/main/java/frequency/DWT/DWTEncoder.java
+++ src/main/java/frequency/DWT/DWTEncoder.java
@@ -1,11 +1,7 @@
 package frequency.DWT;
 
-import jwave.Transform;
-import jwave.transforms.FastWaveletTransform;
-import jwave.transforms.wavelets.haar.Haar1;
 import lib.BitBuilder;
 import lib.BitIterator;
-import lib.TypeConverter;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -22,8 +18,6 @@ import java.util.Optional;
 public class DWTEncoder {
     private String imageFileName;
     private Optional<BufferedImage> stegoImage = Optional.empty();
-    private int p = 2; //block width
-    private int k = 6; //for case 3, we will embed bits in all segments of the 3 x 2 block
 
     public DWTEncoder(String imageFileName) {
         this.imageFileName = imageFileName;
@@ -48,7 +42,7 @@ public class DWTEncoder {
      * @throws IOException
      */
     public void Encode(String message) throws IOException {
-        //We do not need to construct the decimal array, as the iterator handles this
+        message += BitIterator.END_DELIMITER;
         BitIterator B = new BitIterator(message);
 
         BufferedImage C = ImageIO.read(new File(this.imageFileName)); //size is MxN
@@ -56,26 +50,24 @@ public class DWTEncoder {
         int height = C.getHeight();
 
         //Embed all of the message data into the transformed image blocks
-
-        //for component <- {R: 0xfffeffff, G: 0xfffffeff, B: 0xfffffffe}
-        //    offset <- {R: 16, G: 8, B: 0}:
-        //  while B is not empty and I has next:
-        //      block <- [[next HH pixel, next HH pixel],
-        //                [next HL pixel, next HL pixel],
-        //                [next LH pixel, next LH pixel]]
-        //      for segment <- block:
-        //          segment = (segment & component) | (nextBit(B) << offset)
-
         int[][] coefficients = forwardHaar(C);
 
         encodingLoop:
-        for(int j = height / 2; j < height; j++) {
-            for(int i = width/2; i < width; i++) {
-                if(B.hasNext() == false) break encodingLoop;
+        for (int j = height / 2; j < height; j++) {
+            for (int i = width/2; i < width; i++) {
+                if (B.hasNext() == false) break encodingLoop;
 
-                coefficients[i][j] = (coefficients[i][j] & 0xFFFFFFFE) | B.next();
+                int nextByte = B.next();
+                int coefficientParody = coefficients[i][j] & 0x00000001;
 
-                System.out.println(coefficients[i][j]);
+                if (coefficientParody == 1 && nextByte == 0) {
+                    //effectively adding two instead of subtracting one
+                    //done to preserve value through haar inverse
+                    coefficients[i][j] += 1;
+                }
+                else if (coefficientParody == 0 && nextByte == 1) {
+                    coefficients[i][j] += 1;
+                }
             }
         }
 
@@ -83,11 +75,11 @@ public class DWTEncoder {
         //Inverted image is the final stego image with the message embedded in the DWT values but not the final RGB
         this.stegoImage = Optional.of(reverseHaar(C, coefficients));
 
-        test(coefficients, height, width);
+//        test(coefficients, height, width);
     }
 
     private int[][] forwardHaar(BufferedImage C) {
-        double[][] pixelData = new double[C.getHeight()][C.getWidth()];
+        int[][] pixelData = new int[C.getHeight()][C.getWidth()];
 
         //get the pixel data to transform
         for (int row = 0; row < C.getHeight(); row++) {
@@ -98,53 +90,23 @@ public class DWTEncoder {
             }
         }
 
-        //apply the haar function
-        Transform t = new Transform( new FastWaveletTransform( new Haar1()));
-        pixelData = t.forward(pixelData, 1, 1);
-
-        return TypeConverter.doubleMatrixToIntMatrix(pixelData);
+        return HaarTransformer.forward(pixelData);
     }
 
     private BufferedImage reverseHaar(BufferedImage C, int[][] coefficients) {
         //apply the haar function
-        Transform t = new Transform( new FastWaveletTransform( new Haar1()));
-        double[][] pixelData = t.reverse(TypeConverter.intMatrixToDoubleMatrix(coefficients), 1, 1);
+        int[][] pixelData = HaarTransformer.reverse(coefficients);
 
         for (int row = 0; row < C.getHeight(); row++) {
             for (int col = 0; col < C.getWidth(); col++) {
                 //for now, just take the B plane
                 //TODO
-                int newColor = 0xFF000000 + ((int) pixelData[col][row] << 16) + ((int) pixelData[col][row] << 8) + ((int) pixelData[col][row]);
+                int newColor = 0xFF000000 + (pixelData[col][row] << 16) + (pixelData[col][row] << 8) + (pixelData[col][row]);
                 C.setRGB(col, row, newColor);
             }
         }
 
         return C;
-    }
-
-    //TODO Remove
-    //Tests how the transform affects the coefficients
-    private void test(int[][] coefficients, int height, int width) {
-        Transform t = new Transform( new FastWaveletTransform( new Haar1()));
-        double[][] pixelData = t.reverse(TypeConverter.intMatrixToDoubleMatrix(coefficients), 1, 1);
-
-        coefficients = TypeConverter.doubleMatrixToIntMatrix(t.forward(pixelData, 1, 1));
-        BitBuilder result = new BitBuilder();
-        int b = 0x00;
-
-        encodingLoop:
-        for(int j = height / 2; j < height; j++) {
-            for(int i = width/2; i < width; i++) {
-                System.out.println(coefficients[i][j]);
-
-                b = (coefficients[i][j] & 0x00000001);
-
-                if(result.append((byte) b)) break encodingLoop;
-            }
-        }
-
-        //return final string formed from bytes
-        System.out.println(result.toString());
     }
 
     private BufferedImage visualizeHaar(BufferedImage C, int[][] coeffs) {
@@ -156,6 +118,7 @@ public class DWTEncoder {
                 }
             }
         }
+
         //create the haar image
         for (int row = 0; row < coeffs.length; row++) {
             for (int col = 0; col < coeffs[0].length; col++) {
@@ -178,22 +141,11 @@ public class DWTEncoder {
         int width = S.getWidth();
         int height = S.getHeight();
 
-        //for component <- {R: 0x00010000, G: 0x00000100, B: 0x00000001}
-        //    offset <- {R: 16, G: 8, B: 0}:
-        //  while I has next and End Delimiter has not been reached:
-        //      block <- [[next HH pixel, next HH pixel],
-        //                [next HL pixel, next HL pixel],
-        //                [next LH pixel, next LH pixel]]
-        //      for segment <- block:
-        //          result.append(segment & component) >> offset)
-
         int[][] coefficients = forwardHaar(S);
 
         encodingLoop:
         for(int j = height / 2; j < height; j++) {
             for(int i = width/2; i < width; i++) {
-                System.out.println(coefficients[i][j]);
-
                 b = (coefficients[i][j] & 0x00000001);
 
                 if(result.append((byte) b)) break encodingLoop;
--- src/main/java/warden/DST/Main.java
+++ src/main/java/warden/DST/Main.java
@@ -1,56 +1,56 @@
-package warden.DST;
-
-import org.apache.commons.cli.*;
-import warden.RawQuickPair.RawQuickPair;
-
-import java.io.IOException;
-import java.text.ParseException;
-
-public class Main {
-    public static void main(String[] args) {
-
-        Options options = CreateCmdLineOptions();
-        CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd;
-
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-            formatter.printHelp("Bit deletion warden", options);
-
-            System.exit(1);
-            return;
-        }
-
-        String inputFilePath = cmd.getOptionValue("input");
-        String outputFilePath = cmd.getOptionValue("output");
-
-        try {
-            RunWarden(inputFilePath, outputFilePath);
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
-    }
-
-    private static Options CreateCmdLineOptions() {
-        Options options = new Options();
-
-        Option input = new Option("i", "input", true, "input image path");
-        input.setRequired(true);
-        options.addOption(input);
-
-        Option output = new Option("o", "output", true, "output image path");
-        output.setRequired(false);
-        options.addOption(output);
-
-        return options;
-    }
-
-    private static void RunWarden(String inputFilePath, String outputFilePath) throws IOException {
-        DiscreteSpringTransform warden = new DiscreteSpringTransform(inputFilePath);
-        warden.WriteImage(outputFilePath);
-    }
-}
+//package warden.DST;
+//
+//import org.apache.commons.cli.*;
+//import warden.RawQuickPair.RawQuickPair;
+//
+//import java.io.IOException;
+//import java.text.ParseException;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Options options = CreateCmdLineOptions();
+//        CommandLineParser parser = new DefaultParser();
+//        HelpFormatter formatter = new HelpFormatter();
+//        CommandLine cmd;
+//
+//        try {
+////            cmd = parser.parse(options, args);
+//        } catch (ParseException e) {
+//            System.out.println(e.getMessage());
+//            formatter.printHelp("Bit deletion warden", options);
+//
+//            System.exit(1);
+//            return;
+//        }
+//
+//        String inputFilePath = cmd.getOptionValue("input");
+//        String outputFilePath = cmd.getOptionValue("output");
+//
+//        try {
+//            RunWarden(inputFilePath, outputFilePath);
+//        } catch (IOException e) {
+//            System.err.println(e.getMessage());
+//            System.exit(1);
+//        }
+//    }
+//
+//    private static Options CreateCmdLineOptions() {
+//        Options options = new Options();
+//
+//        Option input = new Option("i", "input", true, "input image path");
+//        input.setRequired(true);
+//        options.addOption(input);
+//
+//        Option output = new Option("o", "output", true, "output image path");
+//        output.setRequired(false);
+//        options.addOption(output);
+//
+//        return options;
+//    }
+//
+//    private static void RunWarden(String inputFilePath, String outputFilePath) throws IOException {
+//        DiscreteSpringTransform warden = new DiscreteSpringTransform(inputFilePath);
+////        warden.WriteImage(outputFilePath);
+//    }
+//}
