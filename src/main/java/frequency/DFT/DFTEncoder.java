e4253162186ac01c080d5ccb804548349158abcb
James Vaughn <james.thomas.vaughn@gmail.com>
2018-04-18 18:38:02 -0400

    Added encoding and decoding

--- src/main/java/frequency/DFT/DFTEncoder.java
+++ src/main/java/frequency/DFT/DFTEncoder.java
@@ -52,8 +52,66 @@ public class DFTEncoder {
         BufferedImage C = ImageIO.read(new File(this.imageFileName));
         int width = C.getWidth();
         int height = C.getHeight();
+
+        int[][] coefficients = forwardFourier(C, 0x000000FF, 0);
+
+        encodingLoop:
+        for(int col = 0; col < height; col += 2) {
+            for (int row = 0; row < width; row += 2) {
+                if(B.hasNext() == false) {
+                    break encodingLoop;
+                }
+
+                int nextByte = B.next();
+
+                if(nextByte == 0) {
+                    for(int b = col; b <= col + 1; b++) {
+                        for(int a = row; a <= row + 1; a++) {
+                            coefficients[a][b] = (coefficients[a][b] / 2) * 2;
+                        }
+                    }
+                } else {
+                    for(int b = col; b <= col + 1; b++) {
+                        for(int a = row; a <= row + 1; a++) {
+                            coefficients[a][b] = ((coefficients[a][b] / 2) * 2) + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        C = reverseFourier(C, coefficients, 0x000000FF);
+        this.stegoImage = Optional.of(C);
     }
 
+    private int[][] forwardFourier(BufferedImage C, Integer mask, int shiftAmt) {
+        int[][] pixelData = new int[C.getHeight()][C.getWidth()];
+
+        //get the pixel data to transform with the color component from the mask
+        for (int row = 0; row < C.getHeight(); row++) {
+            for (int col = 0; col < C.getWidth(); col++) {
+                pixelData[col][row] = (C.getRGB(col, row) & mask) >> shiftAmt;
+            }
+        }
+
+        return FourierTransformer.forward(pixelData);
+    }
+
+    private BufferedImage reverseFourier(BufferedImage C, int[][] coefficients, Integer mask) {
+        //apply the haar function
+        int[][] pixelData = FourierTransformer.reverse(coefficients);
+
+        for (int row = 0; row < C.getHeight(); row++) {
+            for (int col = 0; col < C.getWidth(); col++) {
+                //int oldColor = C.getRGB(col, row);
+                int newColor = 0xFF000000 + (pixelData[col][row] << 16) + (pixelData[col][row] << 8) + (pixelData[col][row]);
+                //newColor = (newColor & (0xFF000000 | mask)) | (oldColor & ~(0xFF000000 | mask));
+                C.setRGB(col, row, newColor);
+            }
+        }
+
+        return C;
+    }
 
     public String Decode() throws IOException {
         BufferedImage S = ImageIO.read(new File(this.imageFileName));
@@ -67,6 +125,19 @@ public class DFTEncoder {
         int width = S.getWidth();
         int height = S.getHeight();
 
-        return "";
+
+        int mask = 0x000000FF;
+        int[][] coefficients = forwardFourier(S, mask, 0);
+
+        encodingLoop:
+        for (int j = 0; j < height; j+=2) {
+            for (int i = width; i < width; i+=2) {
+                b = (coefficients[i][j] & 0x00000001);
+
+                if (result.append((byte) b)) break encodingLoop;
+            }
+        }
+
+        return result.toString();
     }
 }
