7a38d02f3a66932c79233cb22d104c9ae04ba71f
Andy Krieger <awk28@case.edu>
2018-04-18 18:35:06 -0400

    Added 2x2 block transform

--- src/main/java/frequency/DFT/FourierTransformer.java
+++ src/main/java/frequency/DFT/FourierTransformer.java
@@ -4,20 +4,27 @@ public class FourierTransformer {
 
     public static int[][] forward(int[][] input) {
         return input;
+
+
+        //
     }
 
     //partially from https://en.wikipedia.org/wiki/Discrete_wavelet_transform#Code_example
     //only performs 1 level
-    private static int[] forward(int[] input) {
-        return input;
+    private static int[][] transform2x2block(int[][] input) {
+
+        int[][] result = new int[4][4];
+
+        result[0][0] = (input[0][0] + input[0][1] + input[1][0] + input[1][1]) / 2;
+        result[0][1] = (input[0][0] - input[0][1] + input[1][0] - input[1][1]) / 2;
+        result[1][0] = (input[0][0] + input[0][1] - input[1][0] - input[1][1]) / 2;
+        result[0][1] = (input[0][0] - input[0][1] - input[1][0] + input[1][1]) / 2;
+
+        return result;
     }
 
     public static int[][] reverse(int[][] input) {
         return input;
     }
 
-    //only performs 1 level
-    private static int[] reverse(int[] input) {
-        return input;
-    }
 }
